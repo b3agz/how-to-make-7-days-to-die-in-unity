5a385d7ff61ed037d7124673429049b649729fed
beagrie <38009915+beagrie@users.noreply.github.com>
2019-11-21 06:48:06 +0000

    Added files from part two (dual contouring)

---
+++ 02-dual-contouring/FlyingCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingCamera : MonoBehaviour {
+
+    public Camera cam;
+
+    private void Update() {
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + (cam.transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal")), Time.deltaTime * 10f);
+        transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0));
+        cam.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 1f));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+
+                if (hit.transform.tag == "Terrain")
+                    hit.transform.GetComponent<Marching>().PlaceTerrain(hit.point);
+                
+            }
+
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 1f));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+
+                if (hit.transform.tag == "Terrain")
+                    hit.transform.GetComponent<Marching>().RemoveTerrain(hit.point);
+                
+            }
+
+        }
+
+    }
+
+}
